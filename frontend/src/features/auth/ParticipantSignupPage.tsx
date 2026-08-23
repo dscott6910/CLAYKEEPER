ef38eae348a91ef7e012172543ada83428c4ef0d
dscott6910 <dscott6910@me.com>
2026-08-23 13:43:51 -0700

    Use grade dropdown in youth registration form

--- frontend/src/features/auth/ParticipantSignupPage.tsx
+++ frontend/src/features/auth/ParticipantSignupPage.tsx
@@ -628,14 +628,24 @@ export function ParticipantSignupPage() {
                   autoComplete="bday"
                 />
 
-                <SignupInput
+                <FormSelect
                   label="Grade as of 2026-2027 school year"
-                  type="number"
                   value={graduationYear}
                   onChange={setGraduationYear}
-                  icon={CalendarDays}
-                  autoComplete="off"
-                  inputMode="numeric"
+                  options={[
+                    "",
+                    "3rd",
+                    "4th",
+                    "5th",
+                    "6th",
+                    "7th",
+                    "8th",
+                    "9th",
+                    "10th",
+                    "11th",
+                    "12th",
+                  ]}
+                  required
                 />
               </div>
 
